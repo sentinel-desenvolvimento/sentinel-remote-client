80d1373cb4d9f4f15227ea76465c69dd0bfd7bc7
Tiago Vidal <tiagovidal@gmail.com>
2026-07-23 23:28:23 -0300

    brand: hide Address Book + Devices tabs (no login/Pro on home)

--- flutter/lib/models/peer_tab_model.dart
+++ flutter/lib/models/peer_tab_model.dart
@@ -41,8 +41,8 @@ class PeerTabModel with ChangeNotifier {
     true,
     true,
     !isWeb && bind.mainGetLocalOption(key: "disable-discovery-panel") != "Y",
-    !(bind.isDisableAb() || bind.isDisableAccount()),
-    !(bind.isDisableGroupPanel() || bind.isDisableAccount()),
+    false, // Lista de enderecos (login/Pro) desativada - Sentinel Remote
+    false, // Dispositivos acessiveis (login/Pro) desativado - Sentinel Remote
   ]);
   final List<bool> _isVisible = List.filled(maxTabCount, true, growable: false);
   List<bool> get isVisibleEnabled => () {
