ce5b02b08455a91a88b676f7d2e8fda648eb4ac0
Tiago Vidal <tiagovidal@gmail.com>
2026-07-23 21:00:07 -0300

    brand: EULA/privacy link -> sentinel.dev.br

--- flutter/lib/desktop/pages/install_page.dart
+++ flutter/lib/desktop/pages/install_page.dart
@@ -187,9 +187,9 @@ class _InstallPageBodyState extends State<_InstallPageBody>
                           InkWell(
                             hoverColor: Colors.transparent,
                             onTap: () => launchUrlString(
-                                'https://rustdesk.com/privacy.html'),
+                                'https://sentinel.dev.br/privacidade'),
                             child: Tooltip(
-                              message: 'https://rustdesk.com/privacy.html',
+                              message: 'https://sentinel.dev.br/privacidade',
                               child: Row(children: [
                                 Icon(Icons.launch_outlined, size: 16)
                                     .marginOnly(right: 5),
